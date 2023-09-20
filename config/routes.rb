d7c70b62c89b3a1f8c69daf85ffaad89ccf28c78
Alexei Danchenkov <adanchenkov@emailline.net>
2023-09-20 12:51:46 -0700

    add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'about/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # resources :about, only: [:index]
+  get :about, to: "about#index"
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "about#index"
 end
