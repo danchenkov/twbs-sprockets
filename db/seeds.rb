940c54a1e68fad4eddaa2ac0cacd3bb080ea937d
Alexei Danchenkov <adanchenkov@emailline.net>
2023-09-20 15:27:49 -0700

    add posts scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.create(
+  title: "First post",
+  body:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam blandit tincidunt iaculis. Suspendisse eleifend lorem tellus, et pellentesque elit pulvinar ac. Mauris nulla tortor, lacinia in dolor a, ullamcorper tristique ligula. Curabitur mollis a urna quis condimentum. Praesent quis nibh odio. Curabitur elementum luctus feugiat. Morbi sit amet laoreet nibh. Nulla libero urna, ullamcorper nec urna ut, semper rhoncus tortor. Nullam et orci quam."
+)
