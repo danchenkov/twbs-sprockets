7b3bd6743311626ff63826782c1169ebd2030779
Alexei Danchenkov <adanchenkov@emailline.net>
2023-09-23 11:48:40 -0700

    add views to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_210333) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_184204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_210333) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
