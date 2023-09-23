78564f223874170decedffc64d5f8d65b3594440
Alexei Danchenkov <adanchenkov@emailline.net>
2023-09-22 21:00:29 -0700

    error handling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,6 +6,17 @@ import "popper"
 import "bootstrap"
 
 // Note that this does not work with turbo reloading
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
+document.addEventListener("turbo:load", function () {
+	const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+	const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+	// const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+	// const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+	// popoverList
+	// let frame = document.getElementById("messagesFrame");
+	// if (frame.complete) {
+	//  frame.scrollTo(0, frame.scrollHeight)
+	// } else {
+	//   frame.loaded.then(() => frame.scrollTo(0, frame.scrollHeight))
+	// }
+})
